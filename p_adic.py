84ac471f3e151c769932d85fbaabd641ebbedb23
quryu-1 <85022205+quryu-1@users.noreply.github.com>
2021-06-11 05:48:53 +0900

    Update p_adic.py

--- p_adic.py
+++ p_adic.py
@@ -1,4 +1,7 @@
 import math
+import time
+
+start = time.time()
 
 def get_num_divisible(n, p):
     i = 1
@@ -14,29 +17,38 @@ def get_inv_elem(n, p):
             return i
 
 def get_p_adic(frac, p, prec=10):
-    l = []
+    if frac[1] == 0:
+        return False
     index_dict = {}
     p_pow = get_num_divisible(frac[0], p) - get_num_divisible(frac[1], p)
-    for i in range(p_pow, 10):
+    i = p_pow
+    while i < prec:
+        print(frac)
+        if frac[0] == 0:
+            index_dict[i] = 0
+            i += 1
+            continue
         p_pow = get_num_divisible(frac[0], p) - get_num_divisible(frac[1], p)
         if p_pow == i:
             if p_pow > 0:
                 beta = [int(frac[0] / p ** p_pow), frac[1]]
             elif p_pow < 0:
-                beta = [frac[0], int(frac[1] / p ** (- p_pow))]
+                beta = [frac[0], int(frac[1] / p ** - p_pow)]
             else:
                 beta = frac
             x = get_inv_elem(beta[1], p)
             a = beta[0] * x % p
-            l.append(a)
             index_dict[i] = a
-            frac = [(beta[0] - a * frac[1]) * p ** p_pow, frac[1]]
+            frac = [int((beta[0] - a * beta[1]) * p ** p_pow), beta[1]]
         else:
-            l.append(0)
             index_dict[i] = 0
-    return l, index_dict
+        i += 1
+        print(index_dict)
+    return index_dict
 
 def convert_to_formula(index_dict, p):
+    if not index_dict:
+        return False
     f = ''
     for k, v in index_dict.items():
         if v > 1:
@@ -45,11 +57,16 @@ def convert_to_formula(index_dict, p):
             f += str(p) + '^' + str(k) + ' + '
         elif v  == 0:
             pass
-    return f.strip(' + ')
+    f += f'O({p}^{list(index_dict.keys())[-1] + 1})'
+    return f
 
 if __name__ == '__main__':
-    p = 3
-    prec = 10
-    p_adic, index_dict = get_p_adic([2, 5], p, prec)
+    p = 5
+    prec = 11
+    frac = [5, 1]
+    index_dict = get_p_adic(frac, p, prec)
     f = convert_to_formula(index_dict, p)
-    print(f)
+    end = time.time()
+    print('p = {}, alpha = {}/{} のp進展開:\n  {}'.format(p, frac[0], frac[1], f))
+    print('処理時間: {}'.format(end - start))
+
